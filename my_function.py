f75255e183747274a44bb34acc7455ac70c78e7d
MnRojas <68394846+mnrojas2@users.noreply.github.com>
2022-04-18 16:39:32 -0400

    Add documentation to my_Function.py

--- my_function.py
+++ my_function.py
@@ -1,21 +1,42 @@
-# Se deben agregar las librerias necesarias para correr la funcion en cada archivo
+"""
+Python module for external functions
+
+#. WARNING: You must add all the necessary libraries here to make the function work in the main loop.
+#. This file includes an example with a gain function. It only scale the input with the value it's set in the parameter 'gain'.
+
+"""
 import numpy as np
 
 #Funcion principal (para la ejecución)
 def my_function(time, inputs, params):
-    # Funcion ganancia
+    """
+    External function 'my_function'
+    
+    :param time: Time value for the main loop.
+    :param inputs: Dictionary with all the input values
+    :param params: Dictionary with all the necessary parameters for the function 
+    :type time: float 
+    :type inputs: dict{numpy.darray}
+    :type params: dict{str}
+    :return: Dictionary with the output(s) as float or numpy vector
+    :rtype: dict{numpy.darray}
+    """
+    
     return {0: np.array(params['gain']*inputs[0])}
 
 #Funcion para inicializar los datos y parámetros necesarios para el bloque
 def _init_():
+    """
+    External function initialization data
+    """
     io_data = {
         'run_ord': 2,
         'inputs': 1,
         'outputs': 1,
-    }
+    } #. Dictionary with the block type, number of inputs and number of outputs.
     params = {
         'gain': 1.5
-    }
+    } #. Dictionary with the necessary parameters for the function.
     return io_data, params
 
 #cada archivo tiene 2 funciones, un ejecutable y un inicializador
